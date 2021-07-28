a61dad6985507606bbe7e712c9eab67b032f5590
Jeremy Meier <jeremy@the-meiers.com>
2021-07-28 08:08:26 -0500

    First attempt at minizing power consumption of script while running on battery

--- tri_sensor.ino
+++ tri_sensor.ino
@@ -7,15 +7,16 @@
 #include <ArduinoJson.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <ArduinoLowPower.h>
+#include <Battery.h>
 
 #include "config.h"
 
-#define DHT_PIN   6
-#define DHT_TYPE  DHT22
+#define DHT_PIN 7
+#define DHT_TYPE DHT22
 
-#define FW_VERSION "0.0.2"
+#define FW_VERSION "0.1.0"
 
-// MKR1010 onboard RTC
 RTCZero rtc = RTCZero();
 
 WiFiUDP ntpUDP = WiFiUDP();
@@ -27,27 +28,44 @@ DHT dht(DHT_PIN, DHT_TYPE);
 
 char clientId[13];
 
-char temperature_config_topic[60];
-char humidity_config_topic[60];
 char base_topic[60];
 char state_topic[60];
 char availability_topic[60];
 
 char sensor_id_t[16];
 char sensor_id_h[16];
+char sensor_id_i[16];
+char sensor_id_b[16];
+
+char temperature_config_topic[60];
+char humidity_config_topic[60];
+char illuminance_config_topic[60];
+char battery_config_topic[60];
 
 String temperature_disc_payload;
 String humidity_disc_payload;
+String illuminance_disc_payload;
+String battery_disc_payload;
 
-unsigned long last_update_ms = 0;
-unsigned long update_interval_ms = 60000;
+unsigned long update_interval_ms = 5 * 60 * 1000; // 5 minutes
+
+int BAT_MIN_MV = 3200;
+int BAT_MAX_MV = 4100;
+float DIVIDER_RATIO = (1200 + 330) / 1200 // From MKR1010 Schematic -> See R8 & R9
+int ADC_REF_VOLTAGE = 3300;
+
+Battery battery(BAT_MIN_MV, BAT_MAX_MV, ADC_BATTERY);
 
 void setup() {
-  setRgbLed(0xFF, 0xFF, 0x0);
-
   Serial.begin(9600);
   delay(3000);
   Serial.println();
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  Serial.print("WiFi Firmware: ");
+  Serial.println(WiFi.firmwareVersion());
 
   byte mac[6];
   WiFi.macAddress(mac);
@@ -60,48 +78,65 @@ void setup() {
   buildDiscoveryPayloads();
 
   wifiConnect();
-  ntpClockUpdate();
-  syncClockToRtc();
-  mqttConnect();
 
-  // Systems go.
-  setRgbLed(0, 0xFF, 0);
-
-  dht.begin();
+  if (WiFi.status() == WL_CONNECTED) {
+    ntpClockUpdate();
+    syncClockToRtc();
+    mqttConnect();
+    mqttPublishDiscovery();
+  }
+  else {
+    Serial.println("Can't start due to failure to connect to WiFi network.");
+    wifiDisconnect();
+    LowPower.deepSleep();
+  }
 }
 
 void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  
+  battery = Battery(BAT_MIN_MV, BAT_MAX_MV, ADC_BATTERY);
+  battery.begin(ADC_REF_VOLTAGE, DIVIDER_RATIO, &sigmoidal);
+  
   mqtt.loop();
 
-  if (!mqtt.connected()) {
+  if (WiFi.status() != WL_CONNECTED) {
     wifiConnect();
+  }
+  
+  if (WiFi.status() == WL_CONNECTED && !mqtt.connected()) {
     mqttConnect();
   }
 
-  if (millis() - last_update_ms > update_interval_ms) {
-    last_update_ms = millis();
-
+  if (mqtt.connected()) {
+    syncClockToRtc();
+    
+    dht.begin();
+    
     StaticJsonDocument<200> doc;
+    
+    int illum_val = 100 * analogRead(A1) / 1023;
+    char ill_str[6];
+    
+    dtostrf(illum_val, 5, 1, ill_str);
+    
     doc["time"] = iso8601_date();
     doc["temperature"] = dht.readTemperature();
     doc["humidity"] = dht.readHumidity();
-
+    doc["illuminance"] = ill_str; // Just a percentage of full scale for now.
+    doc["battery"] = battery.level();
+    
     String msg;
     serializeJson(doc, msg);
     mqtt.publish(state_topic, msg);
+    Serial.print("Publishing message: ");
     Serial.println(msg);
   }
 
-  delay(10);
-}
+  wifiDisconnect();
+  digitalWrite(LED_BUILTIN, LOW);
 
-void setRgbLed(uint8_t red, uint8_t green, uint8_t blue) {
-  WiFiDrv::pinMode(25, OUTPUT);
-  WiFiDrv::pinMode(26, OUTPUT);
-  WiFiDrv::pinMode(27, OUTPUT);
-  WiFiDrv::analogWrite(25, green);
-  WiFiDrv::analogWrite(26, red);
-  WiFiDrv::analogWrite(27, blue);
+  LowPower.deepSleep(update_interval_ms);
 }
 
 time_t syncClock() {
@@ -116,10 +151,13 @@ void mac2Char(byte mac[], char str[]) {
   }
 }
 
-bool wifiConnect() {
-  Serial.print("Connecting to WiFI network..");
+void wifiConnect() {
+  Serial.print("Connecting to WiFi network..");
 
-  int maxWifiAttempts = 30;
+  WiFi.lowPowerMode();
+  
+  int maxWifiAttempts = 5;
+  
   int i = 0;
   while ( WiFi.status() != WL_CONNECTED && i < maxWifiAttempts) {
     WiFi.disconnect();
@@ -130,16 +168,22 @@ bool wifiConnect() {
     i++;
   }
 
-  if (WiFi.status() != WL_CONNECTED) {
-    setRgbLed(0xFF, 0x0, 0x0);
+  if (WiFi.status() != WL_CONNECTED) {  
     Serial.println("Failed");
-    return false;
+    Serial.print("Reason code: ");
+    Serial.println(WiFi.reasonCode());
+    return;
   }
 
-  setRgbLed(0xFF, 0, 0xFF);
   Serial.println("Success!");
+}
 
-  return true;
+void wifiDisconnect() {
+  Serial.println("Disconnecting from WiFi network.");
+  WiFi.disconnect();
+  delay(1000);
+  WiFi.end();
+  delay(1000);
 }
 
 void ntpClockUpdate() {
@@ -156,19 +200,19 @@ void ntpClockUpdate() {
 }
 
 void syncClockToRtc() {
-  setSyncInterval(300); // Seconds between resync of system clock to RTC
+  Serial.print("Sync system clock to RTC..");
+  setSyncInterval(43200); // Seconds between resync of system clock to RTC.  43200 = 12h
   setSyncProvider(syncClock); // Tells system clock how to sync
 
   timeStatus() == timeSet ? Serial.println("Success!") : Serial.println("Failed");
-
-  setRgbLed(0, 0xFF, 0xFF);
 }
 
 bool mqttConnect() {
   Serial.print("Connecting to messaging server..");
 
   mqtt.begin(MQTT_HOST, MQTT_PORT, net);
-  mqtt.setKeepAlive(300);
+  mqtt.setKeepAlive(1800); // 1800 seconds = 30 minutes
+  // The will message here tells Home Assistant the device status is 'offline' if it can't be reached.
   mqtt.setWill(availability_topic, "offline", true, 1);
 
   int max_attempts = 30;
@@ -180,20 +224,20 @@ bool mqttConnect() {
   }
 
   if (!mqtt.connected()) {
-    setRgbLed(0xFF, 0, 0);
     Serial.println("Failed");
     return false;
   }
 
   Serial.println("Success!");
 
-  mqttPublishDiscovery();
   mqtt.publish(availability_topic, "online", true, 1);
 
   return true;
 }
 
 void mqttPublishDiscovery() {
+  Serial.println("Publishing MQTT Discovery payloads for sensor.");
+  
   if (!mqtt.connected()) {
     Serial.println("Can't publish discovery because mqtt isn't connected.");
     return;
@@ -201,6 +245,8 @@ void mqttPublishDiscovery() {
 
   mqtt.publish(temperature_config_topic, temperature_disc_payload, true, 1);
   mqtt.publish(humidity_config_topic, humidity_disc_payload, true, 1);
+  mqtt.publish(illuminance_config_topic, illuminance_disc_payload, true, 1);
+  mqtt.publish(battery_config_topic, battery_disc_payload, true, 1);
 }
 
 void buildTopicNames() {
@@ -211,6 +257,14 @@ void buildTopicNames() {
   strcat(humidity_config_topic, "homeassistant/sensor/logger_");
   strcat(humidity_config_topic, clientId);
   strcat(humidity_config_topic, "_h/config");
+
+  strcat(illuminance_config_topic, "homeassistant/sensor/logger_");
+  strcat(illuminance_config_topic, clientId);
+  strcat(illuminance_config_topic, "_i/config");
+
+  strcat(battery_config_topic, "homeassistant/sensor/logger_");
+  strcat(battery_config_topic, clientId);
+  strcat(battery_config_topic, "_b/config");
 
   strcat(base_topic, "homeassistant/sensor/logger_");
   strcat(base_topic, clientId);
@@ -228,7 +282,7 @@ void buildDiscoveryPayloads() {
   StaticJsonDocument<512> temperature_doc;
   temperature_doc["~"] = base_topic;
   temperature_doc["dev_cla"] = "temperature"; //device_class
-  temperature_doc["name"] = "Logger Temperature";
+  temperature_doc["name"] = "Tri-Sensor Temperature";
   temperature_doc["stat_t"] = "~/state"; //state_topic
   temperature_doc["unit_of_meas"] = "°C"; //unit_of_measurement
   temperature_doc["val_tpl"] = "{{value_json.temperature}}"; //value_template
@@ -244,13 +298,13 @@ void buildDiscoveryPayloads() {
 
   serializeJson(temperature_doc, temperature_disc_payload);
 
-  Serial.println("Temperature discovery:");
+  Serial.print("Temperature discovery:");
   Serial.println(temperature_disc_payload);
 
   StaticJsonDocument<512> humidity_doc;
   humidity_doc["~"] = base_topic;
   humidity_doc["dev_cla"] = "humidity"; //device_class
-  humidity_doc["name"] = "Logger Humidity";
+  humidity_doc["name"] = "Tri-Sensor Humidity";
   humidity_doc["stat_t"] = "~/state"; //state_topic
   humidity_doc["unit_of_meas"] = "%"; //unit_of_measurement
   humidity_doc["val_tpl"] = "{{value_json.humidity}}"; //value_template
@@ -266,8 +320,52 @@ void buildDiscoveryPayloads() {
 
   serializeJson(humidity_doc, humidity_disc_payload);
 
-  Serial.println("Humidity discovery:");
+  Serial.print("Humidity discovery:");
   Serial.println(humidity_disc_payload);
+
+  StaticJsonDocument<512> illuminance_doc;
+  illuminance_doc["~"] = base_topic;
+  illuminance_doc["dev_cla"] = "illuminance"; //device_class
+  illuminance_doc["name"] = "Tri-Sensor Illuminance";
+  illuminance_doc["stat_t"] = "~/state"; //state_topic
+  illuminance_doc["unit_of_meas"] = "%"; //unit_of_measurement
+  illuminance_doc["val_tpl"] = "{{value_json.illuminance}}"; //value_template
+  illuminance_doc["avty_t"] = "~/availability"; //availability_topic
+  illuminance_doc["uniq_id"] = sensor_id_i; //unique_id
+  JsonObject illuminance_dev = illuminance_doc.createNestedObject("dev"); //device
+  illuminance_dev["name"] = "Logger"; //name
+  illuminance_dev["mf"] = "Jeremy Meier"; //manufacturer
+  illuminance_dev["mdl"] = "Tri-Sensor"; //model
+  illuminance_dev["sw"] = FW_VERSION; //sw_version
+  JsonArray illuminance_ids = illuminance_dev.createNestedArray("ids"); //identifiers
+  illuminance_ids.add(clientId);
+
+  serializeJson(illuminance_doc, illuminance_disc_payload);
+
+  Serial.print("Illuminance discovery:");
+  Serial.println(illuminance_disc_payload);
+
+  StaticJsonDocument<512> battery_doc;
+  battery_doc["~"] = base_topic;
+  battery_doc["dev_cla"] = "battery"; //device_class
+  battery_doc["name"] = "Tri-Sensor Battery";
+  battery_doc["stat_t"] = "~/state"; //state_topic
+  battery_doc["unit_of_meas"] = "%"; //unit_of_measurement
+  battery_doc["val_tpl"] = "{{value_json.battery}}"; //value_template
+  battery_doc["avty_t"] = "~/availability"; //availability_topic
+  battery_doc["uniq_id"] = sensor_id_b; //unique_id
+  JsonObject battery_dev = battery_doc.createNestedObject("dev"); //device
+  battery_dev["name"] = "Logger"; //name
+  battery_dev["mf"] = "Jeremy Meier"; //manufacturer
+  battery_dev["mdl"] = "Tri-Sensor"; //model
+  battery_dev["sw"] = FW_VERSION; //sw_version
+  JsonArray battery_ids = battery_dev.createNestedArray("ids"); //identifiers
+  battery_ids.add(clientId);
+
+  serializeJson(battery_doc, battery_disc_payload);
+
+  Serial.print("Battery discovery:");
+  Serial.println(battery_disc_payload);  
 }
 
 void buildSensorIds() {
@@ -276,4 +374,10 @@ void buildSensorIds() {
 
   strcat(sensor_id_h, clientId);
   strcat(sensor_id_h, "_h");
+
+  strcat(sensor_id_i, clientId);
+  strcat(sensor_id_i, "_i");
+
+  strcat(sensor_id_b, clientId);
+  strcat(sensor_id_b, "_b");
 }
